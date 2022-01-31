98e067f7b5dd43e71115200d31a208e6053a9243
Zeon <mushtukov.artem@mail.ru>
2022-01-31 14:01:08 +0300

    pretty print of token added to main

--- main.c
+++ main.c
@@ -1,8 +1,14 @@
-#define ACTIVE_
+#define ACTIVE
 
 #ifdef ACTIVE
 #include <stdio.h>
 #include "input_parse.h"
+
+void printToken(Token * t) {
+    if (t->type == constant) printf("%.2lf%s", t->value, t->imag ? "j" : "");
+    else if (t->type == operation || (t->type == identifier && t->act != none)) printf("%s", RESERVED[t->act]);
+    else printf("var(id:%d)", t->varID);
+}
 
 int main() {
     InputExpression ie = getInput();
@@ -10,7 +16,7 @@ int main() {
     for (int i = 0; i < ie.lineCount; ++i) {
         TokenArray ta = pe.lines[i];
         for (Token *t = ta.array; t < ta.array + ta.size; ++t)
-            printf("%d ", t->type);
+            printToken(t);
         printf("\n");
     }
     printf("variables: %d, lines: %d", pe.varCount, pe.lineCount);
